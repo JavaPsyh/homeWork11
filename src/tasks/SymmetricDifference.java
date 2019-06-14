f5f55845c967d0528ac537592763ee8656a35a85
JavaPsyh <psyhjava@gmail.com>
2019-06-15 01:52:27 +0300

    Commit for verification

--- src/tasks/SymmetricDifference.java
+++ src/tasks/SymmetricDifference.java
@@ -1,4 +1,21 @@
 package tasks;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class SymmetricDifference {
+
+    public static <T> Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
+
+        Set<T> linkdHashSet = new LinkedHashSet<>(set1);
+        System.out.println(GetCallerClassAndMethodName.getCallerClassAndMethodName());
+        for (T value : set2) {
+            if (linkdHashSet.contains(value)) {
+                linkdHashSet.remove(value);
+            } else {
+                linkdHashSet.add(value);
+            }
+        }
+        return linkdHashSet;
+    }
 }

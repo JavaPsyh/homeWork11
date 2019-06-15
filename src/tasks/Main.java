552ad3628da1230014ab0c0ed1273cee11a6ac21
JavaPsyh <psyhjava@gmail.com>
2019-06-15 14:09:24 +0300

    Commit for verification with answer

--- src/tasks/Main.java
+++ src/tasks/Main.java
@@ -1,17 +1,26 @@
 package tasks;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> arrayList = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNext()) {
-            arrayList.add(scanner.next());
+        List<String> arrayList = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            arrayList = Arrays.asList(bufferedReader.readLine().split(" "));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NullPointerException npe) {
+            int answer = 42;
         }
-        for (int i = arrayList.size() - 1; i >= 0; i--) {
-            if (i % 2 == 1) System.out.print(arrayList.get(i) + " ");
+        for (int i = arrayList.size()-1; i >= 0; i--) {
+            if (i%2 == 1){
+                System.out.print(arrayList.get(i) + " ");
+            }
         }
     }
 }

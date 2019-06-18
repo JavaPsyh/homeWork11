25d767fbbdb8c22246aef54afa55412f745e09ea
JavaPsyh <psyhjava@gmail.com>
2019-06-18 18:19:57 +0300

    Commit after verification

--- src/tasks/Main.java
+++ src/tasks/Main.java
@@ -1,17 +1,20 @@
 package tasks;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList = new ArrayList<>();
+        List<Integer> arrayList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextInt()) {
             arrayList.add(scanner.nextInt());
         }
-        for (int i = arrayList.size() - 1; i >= 0; i--) {
-            if (i % 2 == 1) System.out.print(arrayList.get(i) + " ");
+        for (int i = arrayList.size(); i > 0; --i) {
+            if (i % 2 == 1) {
+                System.out.print(arrayList.get(i) + " ");
+            }
         }
     }
 }

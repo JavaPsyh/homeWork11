bda96e9bc536087c324dea51ab8ff3bef9bd97d9
JavaPsyh <psyhjava@gmail.com>
2019-06-20 17:45:23 +0300

    Commit after verification with Scanner, String in one loop

--- src/tasks/Main.java
+++ src/tasks/Main.java
@@ -1,31 +1,20 @@
 package tasks;
 
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
         Scanner scanner = new Scanner(System.in);
-        int number;
-        int count = 0;
-        boolean flag = true;
-        while (flag) {
-            if (scanner.hasNextInt()) {
-                number = scanner.nextInt();
-                if (count % 2 != 0) {
-                    stack.push(number);
-                }
-                count++;
-            } else {
-                if (!stack.empty()) {
-                    System.out.print(stack.pop());
-                }
-                if (!stack.empty()) {
-                    System.out.print(" ");
-                } else {
-                    flag = false;
-                }
+        String numbers = "";
+        if (scanner.hasNextLine()) {
+            numbers = scanner.nextLine();
+        }
+        String[] arrayOfNumbers = numbers.trim().split(" ");
+        int lastOdd = arrayOfNumbers.length % 2 == 0 ? arrayOfNumbers.length-1 : arrayOfNumbers.length-2;
+        for (int i = lastOdd; i > 0; i = i - 2) {
+            System.out.print(arrayOfNumbers[i]);
+            if (i != 1){
+                System.out.print(" ");
             }
         }
     }
